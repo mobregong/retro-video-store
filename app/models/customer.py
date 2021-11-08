9dbf4248b3c936b8260792e75d02b3455bf21d25
Maria Obregon Garcia <mobregong@gmail.com>
2021-11-08 14:50:59 -0500

    Create Customer model with id, name, postal_code, phone_number, registered_at, and  to_dict method

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,18 @@
 from app import db
+import datetime
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable = False)
+    postal_code = db.Column(db.String)
+    phone_number = db.Column(db.String)
+    registered_at = db.Column(db.DateTime(timezone=True), nullable=False, default=datetime.datetime.utcnow())
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "postal_code": self.postal_code,
+            "phone_number": self.phone_number,
+            "registered_at": self.registered_at
+        }
