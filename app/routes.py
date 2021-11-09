8d0c38490da537cbc260e3369f272d41ae24b4df
Ana Gabriele <gabbanasoares@gmail.com>
2021-11-08 18:55:05 -0800

    Create function to delete and update videos

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import re
 from app import db
 from app.models.video import Video
 from datetime import datetime
@@ -20,7 +21,7 @@ def add_video():
             response_body = {"details": 'Request body must include release_date.'}
             return make_response(response_body, 400)
         elif  "total_inventory" not in request_body:
-            response_body = {"details": "Request body must include total_inventory"}
+            response_body = {"details": "Request body must include total_inventory."}
             return make_response(response_body, 400)
         
         new_video = Video(title = request_body["title"], 
@@ -57,10 +58,77 @@ def read_all():
 
 
 '''GET - read one'''
+@video_bp.route("/<video_id>", methods=["GET"])
+def read_one_video(video_id):
+    video = get_video_by_id(video_id)
+
+    try:
+        response_body = video.to_json()
+        return make_response(jsonify(response_body),200)
+    except:
+        abort(400)
+
+
+''' PUT'''
+
+@video_bp.route("/<video_id>", methods=["PUT"])
+def update_video(video_id): 
+    video = get_video_by_id(video_id)
+
+    try:
+        request_body = request.get_json()
+        if not request_body or "title" not in request_body or "release_date" not in request_body or "total_inventory" not in request_body:
+            abort(400)
+        if "title" in request_body:
+            video.title = request_body["title"]
+        if "release_date" in request_body:
+            video.release_date = request_body["release_date"]
+        if "total_inventory" in request_body:
+            video.total_inventory = request_body["total_inventory"]
+        # commit uppdates
+        db.session.commit()
+        response_body = video.to_json()
+        return make_response(response_body, 200)
+    except:
+        abort(400)
+
+
+'''DELETE'''
+@video_bp.route("/<video_id>", methods=["DELETE"])
+def delete_video(video_id): 
+    video = get_video_by_id(video_id)
+    try:
+        # delete entity and commit it
+        
+        db.session.delete(video)
+        db.session.commit()
+
+        response_body ={"id": video.id}
+
+        return make_response(response_body), 200
+
+    except Exception:
+        abort(422)
+
+
+
 
 ''' Helper Functions '''
 
+def get_video_by_id(video_id):
+    id = valid_int(video_id)
+    video = Video.query.filter_by(id=id).one_or_none()    
+    if video is None:
+        response_body = {"message": f"Video {id} was not found"}
+        abort(make_response(response_body, 404))   
+    return video
 
+def valid_int(number):
+    try:
+        id = int(number)
+        return id 
+    except:
+        abort(400)
 
 
 
@@ -70,9 +138,6 @@ def read_all():
 
 '''Error Handlers'''
 
-@video_bp.errorhandler(404)
-def not_found(error):
-    return jsonify({"success": False, "error": 404, "message": "Not found"}),404
 
 @video_bp.errorhandler(400)
 def bad_request(error):
