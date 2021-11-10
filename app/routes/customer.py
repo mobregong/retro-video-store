8fe14ebe4dccf07aa50923ba859a6c75e5682558
Maria Obregon Garcia <mobregong@gmail.com>
2021-11-10 16:17:07 -0500

    Creates customer file within routes folder

---
+++ app/routes/customer.py
@@ -0,0 +1,100 @@
+from app import db
+from app.models.customer import Customer
+from flask import Blueprint, jsonify, request, make_response, abort
+
+from tests.test_wave_01 import CUSTOMER_ID
+
+# Customers
+
+customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
+
+# Helper Functions
+# def valid_customer(customer_id):
+#     try:
+#         int(customer_id)
+#     except:
+#         abort(make_response({"error": "customer_id must be an int"}, 400))
+
+# def get_customer_from_id(customer_id):
+    # valid_customer(customer_id)
+    # selected_customer = Customer.query.get_or_404(customer_id, description=f"Customer {customer_id} was not found")
+    # return selected_customer
+
+def valid_int(number):
+    try:
+        id = int(number)
+        return id 
+    except:
+        abort(400)
+
+def get_customer_from_id(customer_id):
+    id = valid_int(customer_id)
+    customer = Customer.query.filter_by(id=id).one_or_none()    
+    if customer is None:
+        response_body = {"message": f"Customer {id} was not found"}
+        abort(make_response(response_body, 404))   
+    return customer
+
+# Routes
+@customers_bp.route("", methods=["GET"], strict_slashes=False)
+def get_all_customers():
+    customer_objects = Customer.query.all() # returning list of objects
+    response_list = []
+    for customer in customer_objects:
+        response_list.append(customer.to_dict())
+    return make_response(jsonify(response_list), 200)
+
+@customers_bp.route("/<customer_id>", methods=["GET"])
+def get_customer(customer_id):
+    selected_customer = get_customer_from_id(customer_id)
+    # return make_response({"customer": selected_customer.to_dict()}, 200)
+    return make_response(selected_customer.to_dict(), 200)
+
+@customers_bp.route("", methods=["POST"], strict_slashes=False)
+def add_customer():
+    request_body = request.get_json()
+    if "postal_code" not in request_body:
+        response_body = {"details": "Request body must include postal_code."}
+        response = abort(make_response(response_body, 400))   
+    if "name" not in request_body:
+        response_body = {"details": "Request body must include name."}
+        response = abort(make_response(response_body, 400)) 
+    if "phone" not in request_body:
+        response_body = {"details": "Request body must include phone."}
+        response = abort(make_response(response_body, 400))
+    new_customer = Customer(
+        name=request_body["name"],
+        postal_code=request_body["postal_code"],
+        phone=request_body["phone"]
+    )
+    db.session.add(new_customer)
+    db.session.commit() 
+    response = make_response(new_customer.to_dict(), 201)
+    return response
+
+@customers_bp.route("/<customer_id>", methods=["DELETE"])
+def delete_customer(customer_id):
+    selected_customer = get_customer_from_id(customer_id)
+    db.session.delete(selected_customer)
+    db.session.commit()
+    return make_response({"id": int(customer_id)}, 200)
+
+@customers_bp.route("/<customer_id>", methods=["PUT"])
+def update_customer(customer_id):
+    selected_customer = get_customer_from_id(customer_id)
+    request_body = request.get_json()
+    if "name" in request_body:
+        selected_customer.name = request_body["name"]
+    else:
+        abort(make_response({"error": "Invalid input"},400)) 
+    if "phone" in request_body:
+        selected_customer.phone = request_body["phone"]
+    else:
+        abort(make_response({"error": "Invalid input"},400)) 
+    if "postal_code" in request_body:
+        selected_customer.postal_code = request_body["postal_code"]
+    else:
+        abort(make_response({"error": "Invalid input"},400)) 
+    db.session.commit()
+    response_body = jsonify(selected_customer.to_dict())
+    return make_response(response_body, 200)
