b89758507613e953bee5a6d4bfcd74295f139318
Ana Gabriele <gabbanasoares@gmail.com>
2021-11-12 18:52:15 -0800

    refactor videos routes

---
+++ app/routes/helper_functions.py
@@ -0,0 +1,41 @@
+from app import db
+from app.models.customer import Customer
+from flask import Blueprint, jsonify, request, make_response, abort
+from app.models.rental import Rental
+from app.models.video import Video
+
+
+
+
+
+def valid_int(number):
+    try:
+        id = int(number)
+        return id 
+    except:
+        abort(400)
+
+def get_customer_from_id(customer_id):
+    id = valid_int(customer_id)
+    customer = Customer.query.filter_by(id=id).one_or_none()    
+    if customer is None:
+        response_body = {"message": f"Customer {id} was not found"}
+        abort(make_response(response_body, 404))   
+    return customer
+
+def get_video_by_id(video_id):
+    id = valid_int(video_id)
+    video = Video.query.filter_by(id=id).one_or_none()    
+
+    if video is None:
+        response_body = {"message": f"Video {id} was not found"}
+        abort(make_response(response_body, 404))   
+    return video
+
+def get_customer_and_video_id(request_body):
+    customer = get_customer_from_id(request_body['customer_id'])
+    video =  get_video_by_id(request_body['video_id'])
+    return customer,video
+
+
+
--- app/routes/rentals.py
+++ app/routes/rentals.py
@@ -1,29 +1,33 @@
-import sys
 from app import db
-from app.models.customer import Customer
-from app.models.video import Video
 from app.models.rental import Rental
-from app.routes.customers import get_customer_from_id
-from app.routes.videos import get_video_by_id, valid_int
-from flask import Blueprint, jsonify, request, make_response, abort
+from flask import Blueprint,  request, make_response
 from datetime import datetime, timedelta
+from app.routes.helper_functions import * 
 
 rentals_bp = Blueprint('rentals', __name__, url_prefix='/rentals')
 
 
-
 # Routes
 # Post check-out
+'''
+handling request, 
+is it valid?, 
+getting filtered data, 
+checking out, 
+returning data
+'''
 @rentals_bp.route("/check-out", methods=["POST"], strict_slashes=False)
 def check_out_video():
     request_body = request.get_json()
+
     error_message = {"message": "Could not perform checkout"}, 400
 
     if "customer_id" not in request_body or "video_id" not in request_body:
         return make_response(error_message)
 
-    customer = get_customer_from_id(request_body['customer_id'])
-    video =  get_video_by_id(request_body['video_id'])
+    customer, video = get_customer_and_video_id(request_body)
+
+
     videos_checked_out_by_customer_count = Rental.query.filter_by(customer_id=customer.id).count()
     videos_id_checked_out_count = Rental.query.filter_by(video_id=video.id).count()
     available_inventory = video.total_inventory - videos_id_checked_out_count
@@ -48,6 +52,16 @@ def check_out_video():
 
 
 
+
+
+# Post check-in
+'''
+* handling request,
+* is it valid?, 
+* getting filtered data, 
+* handling test_checkin_video_not_checked_out, 
+* checking in rental, returning data
+'''
 @rentals_bp.route("/check-in", methods=["POST"], strict_slashes=False)
 def check_in_video():
 
@@ -56,15 +70,14 @@ def check_in_video():
     if "customer_id" not in request_body or "video_id" not in request_body:
         return make_response({"message": "Could not perform check in"}, 400)
 
-    customer = get_customer_from_id(request_body['customer_id'])
-    video =  get_video_by_id(request_body['video_id'])
-    rentals = Rental.query.filter_by(video_id=video.id, customer_id=customer.id).one_or_none()
-    if rentals == None:
-        return make_response({"message": "No outstanding rentals for customer 1 and video 1"}, 400)
+    customer, video = get_customer_and_video_id(request_body)
 
-# video was not checked in yet 
+    rentals = Rental.query.filter_by(video_id=video.id, customer_id=customer.id).one_or_none()
+
+    if rentals == None:
+        return make_response({"message": f"No outstanding rentals for customer {customer.id} and video {video.id}"}, 400)
+
     if not rentals.checked_in:
-        # update available_inventory and checked ou count
         rentals.due_date = None,
         rentals.available_inventory =  rentals.available_inventory + 1
         rentals.videos_checked_out_count=  rentals.videos_checked_out_count - 1
